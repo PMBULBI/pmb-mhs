cf22d6a43fb89b00dc89fa3e546691dea2504487
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-21 11:23:02 +0700

    feat(alertcicilan) : alert konfirmasi bayar cicilan

--- pembayaran/metodepembayaran.js
+++ pembayaran/metodepembayaran.js
@@ -16,3 +16,22 @@ document.getElementById('buttonLunas').addEventListener('click', function() {
       }
     });
   });
+
+// Event listener untuk button Pembayaran Cicilan
+document.getElementById('buttonCicilan').addEventListener('click', function() {
+    // Tampilkan SweetAlert dengan opsi konfirmasi
+    Swal.fire({
+      title: 'Konfirmasi',
+      text: 'Apakah Anda yakin sudah setuju dengan isi dari pernyataan pada layar Anda? Data yang sudah diinput tidak dapat diubah.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Ya, Lanjutkan!'
+    }).then((result) => {
+      // Jika pengguna mengklik "Ya", arahkan ke halaman pembayaran.html
+      if (result.isConfirmed) {
+        window.location.href = 'metodepembayaran.html';
+      }
+    });
+  });
