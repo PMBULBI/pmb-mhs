2026bbc0ca73b450817383210a5ada6b15da355f
rofinafiin <rofinafiisr@gmail.com>
2023-11-23 14:59:02 +0700

    fix(sidebar): penyesuaian javascript

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,9 +1,5 @@
-
 $(function () {
-    // Load navbar.html on page load
-    $("#sidebar-container").load("static/sidebar.html");
-// Toggle the visibility of the navbar on button click
-    $("#sidebar_type").click(function () {
-        $("#sidebar-container").toggleClass("hidden");
-    });
+  // Load navbar.html on page load
+  $("#sidebar-container").load("static/sidebar.html");
+
 });
