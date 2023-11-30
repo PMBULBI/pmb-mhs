53e285d6b2d64af91660b3a0c2533669884ec922
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-30 15:04:08 +0700

    feat(getdata) : get data prodi 2 dropdown

--- datadiri/pilihprodi.js
+++ datadiri/pilihprodi.js
@@ -62,3 +62,34 @@ function populateDropdownProdi1(data) {
     })
 }
 fetchDataProdi1();
+
+// Get Program Studi 2
+// Membuat fungsi untuk fetch data prodi
+function fetchDataProdi2() {
+    fetch(UrlGetProgramStudi)
+        .then(response => response.json())
+        .then(data => {
+            populateDropdownProdi2(data.data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+// Membuat fungsi dropdown jalur pendaftaran
+function populateDropdownProdi2(data) {
+    const selectDropdown = document.getElementById('selectprog2');
+    selectDropdown.innerHTML = '';
+
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.text = 'Pilih Program Studi 2';
+    selectDropdown.appendChild(defaultOption);
+
+    data.forEach(item => {
+        const option = document.createElement('option');
+        option.value = item.kode_program_studi;
+        option.text = item.program_studi;
+        selectDropdown.appendChild(option);
+    })
+}
+fetchDataProdi2();
