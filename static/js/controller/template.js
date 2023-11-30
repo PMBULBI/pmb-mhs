676bcd4f2f753fefdecd574a5b29ca64722258aa
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-30 15:03:43 +0700

    feat(endpoint) : ketersediaan endpoint mhs

---
+++ static/js/controller/template.js
@@ -0,0 +1,4 @@
+// Endpoint Get
+export let UrlGetJalurPendaftaran = 'https://komarbe.ulbi.ac.id/jalur';
+export let UrlGetFakultas = 'https://komarbe.ulbi.ac.id/fakultas';
+export let UrlGetProgramStudi = 'https://komarbe.ulbi.ac.id/prodi';
