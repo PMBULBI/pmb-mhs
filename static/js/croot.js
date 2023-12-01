7b6961085d52fc61a9cb94a601dd6a7948a163e0
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2023-12-01 14:41:45 +0700

    feat: set data mhs by token

--- static/js/croot.js
+++ static/js/croot.js
@@ -1,5 +1,20 @@
 import { token } from "./controller/cookies.js";
+import { getWithHeader } from "https://jscroot.github.io/api/croot.js";
+import { setInner } from "https://jscroot.github.io/element/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 if (token === "") {
     window.location.replace("https://pmb.ulbi.ac.id");
 }
+
+function main(){
+    getWithHeader("https://komarbe.ulbi.ac.id/pendaftar/pendaftar/token", "LOGIN", getCookie("login"), setDataMhs);
+}
+
+function setDataMhs(result){
+    setInner("email_mhs", result.data.email_mhs);
+    setInner("no_hp", result.data.nama_mhs);
+    setInner("nama_mhs", result.data.hp_mhs);
+}
+
+main();
