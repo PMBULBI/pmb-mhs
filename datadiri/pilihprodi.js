69ed43cfe4fcdb5773f33a189a1b586e58ff30fa
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-02 19:55:05 +0700

    feat(eventlistener) : event listener untuk pilih prodi

--- datadiri/pilihprodi.js
+++ datadiri/pilihprodi.js
@@ -159,3 +159,34 @@ function submitPilihProdi() {
         console.error("Error saat melakukan POST Data : ", error);
     });
 }
+
+// Event listener untuk tombol "submit"
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', () => {
+    const prodiSatu = document.getElementById('selectprog').value;
+    const prodiKedua = document.getElementById('selectprog2').value;
+
+    if (!prodiSatu || !prodiKedua) {
+        Swal.fire({
+            icon: 'warning',
+            title: 'Oops...',
+            text: 'Semua field harus diisi!',
+        });
+        return;
+    }
+    // Add additional validation if needed
+    Swal.fire({
+        title: 'Submit Program Studi?',
+        text: 'Apakah anda yakin ingin submit program studi?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            submitPilihProdi();
+        }
+    });
+})
