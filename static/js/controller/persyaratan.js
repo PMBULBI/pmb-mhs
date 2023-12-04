35daa7795fc93d924b74c98f378c2b18a120d46c
rofinafiin <rofinafiisr@gmail.com>
2023-12-04 12:34:00 +0000

    fix all done

--- static/js/controller/persyaratan.js
+++ static/js/controller/persyaratan.js
@@ -1,44 +1,42 @@
 import { UrlFilePost } from "./template.js";
-import {getValue} from "https://jscroot.github.io/element/croot.js";
+import { getValue } from "https://jscroot.github.io/element/croot.js";
 import { token } from "./cookies.js";
-
-var header = new Headers();
-header.append("login", token);
-header.append("Content-Type", "multipart/form-data");
-
-// Untuk POST prodi & fakultas
-// Membuat fungsi untuk mengirimkan data pilih prodi ke API
 
 // Event listener for the "Submit" button
 const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', () => {
-            const form = document.getElementById("form");
-            const inputFile = document.getElementById("file");
-        
-            const formData = new FormData();
-        
-            const handleSubmit = (event) => {
-                event.preventDefault();
-        
-                for (const file of inputFile.files) {
-                    formData.append("files", file);
-                }
-        
-                // Now, you can call the fetch inside handleSubmit
-                fetch(UrlFilePost, {
-                    method: "POST",
-                    headers: header,
-                    body: formData
-                })
-                .then(response => response.json())
-                .catch(error => {
-                    console.error("Error saat melakukan POST Data : ", error);
-                });
-        
-            // Event listener for form submission
-            form.addEventListener("click", handleSubmit);
+    const form = document.getElementById("form");
+    const inputFile = document.getElementById("file_input");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+
+        for (const file of inputFile.files) {
+            formData.append("file", file);
         }
-    }
-    );
 
+        try {
+            const response = await fetch(UrlFilePost, {
+                method: "POST",
+                headers: {
+                    'login': token,
+                },
+                body: formData,
+            }).then(window.location.replace("https://pmb.ulbi.ac.id/pmb-mhs/VAPage.html"));
 
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Response data:", data);
+        } catch (error) {
+            console.error("Error saat melakukan POST Data:", error);
+        }
+    };
+
+    // Event listener for form submission
+    form.addEventListener("submit", handleSubmit);
+});
