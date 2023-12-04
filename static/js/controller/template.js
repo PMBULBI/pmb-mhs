d6aee4541a7a6800ccafc5e9e96340568f234667
rofinafiin <rofinafiisr@gmail.com>
2023-12-04 07:18:45 +0000

    fix(url): fix url post dan get

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -2,6 +2,9 @@
 export let UrlGetJalurPendaftaran = 'https://komarbe.ulbi.ac.id/jalur';
 export let UrlGetFakultas = 'https://komarbe.ulbi.ac.id/fakultas';
 export let UrlGetProgramStudi = 'https://komarbe.ulbi.ac.id/prodi';
+export let UrlGetProvinsi = 'https://komarbe.ulbi.ac.id/wilayah-provinsi/get';
+export let UrlGetKota = 'https://komarbe.ulbi.ac.id/wilayah-kota/get';
+export let UrlPostDatadiri = 'https://komarbe.ulbi.ac.id/biodata/data_diri';
 
 export const UrlBiodataJalur = 'https://komarbe.ulbi.ac.id/biodata/jalur';
 
