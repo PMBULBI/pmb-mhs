449bb7b4acb17e61c4fc45eb2572f537de255f31
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-05 13:20:02 +0700

    add(getkecamatan) : add endpoint get all kecamatan

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -1,9 +1,12 @@
-// Endpoint Get
+// Endpoint GET
 export let UrlGetJalurPendaftaran = 'https://komarbe.ulbi.ac.id/jalur';
 export let UrlGetFakultas = 'https://komarbe.ulbi.ac.id/fakultas';
 export let UrlGetProgramStudi = 'https://komarbe.ulbi.ac.id/prodi';
 export let UrlGetProvinsi = 'https://komarbe.ulbi.ac.id/wilayah-provinsi/get';
 export let UrlGetKota = 'https://komarbe.ulbi.ac.id/wilayah-kota/get';
+export let UrlGetKecamatan = 'https://komarbe.ulbi.ac.id/wilayah-kecamatan/get';
+
+// Endpoint POST
 export let UrlPostDatadiri = 'https://komarbe.ulbi.ac.id/biodata/data_diri';
 export let UrlPostDataOrtu = 'https://komarbe.ulbi.ac.id/biodata/data_ortu';
 export let UrlPostDataSekolah = 'https://komarbe.ulbi.ac.id/biodata/data_sekolah';
