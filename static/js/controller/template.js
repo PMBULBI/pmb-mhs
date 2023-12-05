5b7f214518049cf6cf77f928469aa1236fc0f592
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-05 22:10:26 +0700

    add(endpoint) : UrlGetKecamatanByIdKotaNmKec

--- static/js/controller/template.js
+++ static/js/controller/template.js
@@ -7,6 +7,7 @@ export let UrlGetKota = 'https://komarbe.ulbi.ac.id/wilayah-kota/get';
 export let UrlGetKecamatan = 'https://komarbe.ulbi.ac.id/wilayah-kecamatan/get';
 export let UrlGetKelurahan = 'https://komarbe.ulbi.ac.id/wilayah-kelurahan/get';
 export let UrlGetKotaByIdProvNmKota = 'https://komarbe.ulbi.ac.id/wilayah-kota/getidprovnmkota';
+export let UrlGetKecamatanByIdKotaNmKec = 'https://komarbe.ulbi.ac.id/wilayah-kecamatan/getidkotanmkecamatan';
 
 // Endpoint POST
 export let UrlPostDatadiri = 'https://komarbe.ulbi.ac.id/biodata/data_diri';
