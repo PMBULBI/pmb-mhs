f9885c2d258b297dac6d6ce7e5c419601d12a237
rofinafiin <rofinafiisr@gmail.com>
2023-12-06 12:05:50 +0000

    va dari status

--- static/js/controller/va.js
+++ static/js/controller/va.js
@@ -1,20 +1,18 @@
-import {get} from "https://jscroot.github.io/api/croot.js";
+import { postWithToken} from "https://jscroot.github.io/api/croot.js";
+import {getValue, addChild} from "https://jscroot.github.io/element/croot.js";
 import { token } from "./cookies.js";
-
-var header = new Headers();
-header.append("login", token);
-header.append("Content-Type", "application/json");
-
-var requestOptions = {
-    method: 'POST',
-    headers: header
-  };
   
 function fetchVA() {
-    fetch("https://komarbe.ulbi.ac.id/va/create", requestOptions)
-    .then(response => response.json())
-    .then(result => result)
-    .catch(error => console.log('error', error));
+    postWithToken("https://komarbe.ulbi.ac.id/va/create", "LOGIN", token,  "test" ,Response)
+}
+
+function Response(value){
+  // console.log(value)
+  if (value.success === false){
+    addChild("cek", "h4", "text-3xl font-medium text-slate-900 dark:text-white mb-2", value.status)
+  } else {
+    addChild("cek", "h4", "text-3xl font-medium text-slate-900 dark:text-white mb-2", value.status)
+  }
 }
 
 fetchVA();
