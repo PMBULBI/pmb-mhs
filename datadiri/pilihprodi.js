f666ba87ada6596ab9544ff0ae8b32634f7fa893
rofinafiin <rofinafiisr@gmail.com>
2023-12-06 13:38:13 +0000

    prodi

--- datadiri/pilihprodi.js
+++ datadiri/pilihprodi.js
@@ -8,46 +8,6 @@ var header = new Headers();
 header.append("login", token);
 header.append("Content-Type", "application/json");
 
-let dataJalur;
-
-// Get Data untuk Navbar
-document.addEventListener("DOMContentLoaded", function() {
-    // Ambil nilai dari cookie dengan nama 'namaMhs'
-    var namaMhsCookie = getCookieData('namaMhs');
-    // Cek apakah cookie ada
-    if (namaMhsCookie) {
-        // Set nilai cookie ke dalam elemen dengan ID 'nama_mhs_span'
-        document.getElementById('nama_mhs_span').innerText = namaMhsCookie;
-    }
-});
-// Fungsi untuk mendapatkan nilai cookie berdasarkan nama
-function getCookieData(name) {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i].trim();
-        if (cookie.indexOf(name + '=') === 0) {
-            return cookie.substring(name.length + 1);
-        }
-    }
-    return null;
-}
-
-// Get Fakultas
-// Membuat fungsi untuk fetch data fakultas
-const setValue = ( res ) =>{
-    if (res.data == null){
-        return;
-    };
-    dataJalur = res;
-    return
-} 
-
-function fetchDataBiodataJalur(){
-    getWithHeader(UrlBiodataJalur, TokenHeader, getCookie(CookieName), setValue);
-}
-
-fetchDataBiodataJalur();
-
 // Untuk POST prodi & fakultas
 // Membuat fungsi untuk mengirimkan data pilih prodi ke API
 function submitPilihProdi() {
@@ -121,6 +81,48 @@ submitButton.addEventListener('click', () => {
         }
     });
 })
+
+let dataJalur;
+
+// Get Data untuk Navbar
+document.addEventListener("DOMContentLoaded", function() {
+    // Ambil nilai dari cookie dengan nama 'namaMhs'
+    var namaMhsCookie = getCookieData('namaMhs');
+    // Cek apakah cookie ada
+    if (namaMhsCookie) {
+        // Set nilai cookie ke dalam elemen dengan ID 'nama_mhs_span'
+        document.getElementById('nama_mhs_span').innerText = namaMhsCookie;
+    }
+});
+// Fungsi untuk mendapatkan nilai cookie berdasarkan nama
+function getCookieData(name) {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i].trim();
+        if (cookie.indexOf(name + '=') === 0) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+// Get Fakultas
+// Membuat fungsi untuk fetch data fakultas
+const setValue = ( res ) =>{
+    if (res.data == null){
+        return;
+    };
+    dataJalur = res;
+    return
+} 
+
+function fetchDataBiodataJalur(){
+    getWithHeader(UrlBiodataJalur, TokenHeader, getCookie(CookieName), setValue);
+}
+
+fetchDataBiodataJalur();
+
+
 
 // Get Program Studi 1
 // Membuat fungsi untuk fetch data prodi
