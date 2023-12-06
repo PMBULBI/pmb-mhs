5624793f58e2729544dbec7f1512d85ee4ee9742
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-06 11:10:27 +0700

    feat(js) : js untuk hal lunas registrasi

---
+++ pembayaran/lunasregistrasi.js
@@ -0,0 +1,12 @@
+// Untuk Cetak Invoice Registrasi
+// function printInvoice() {
+//     // Open a new window or create an iframe
+//     var printWindow = window.open('', '_blank');
+    
+//     // Copy the HTML content of the targeted card to the new window or iframe
+//     var invoiceContent = document.querySelector('.card').outerHTML;
+//     printWindow.document.write('<html><head><title>Invoice</title></head><body>' + invoiceContent + '</body></html>');
+    
+//     // Call the print function in the new window or iframe
+//     printWindow.print();
+//   }
