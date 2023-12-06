18ddbd597756f8eb3119daaf507a02cadfb90d6b
rofinafiin <rofinafiisr@gmail.com>
2023-12-06 12:40:06 +0000

    sekolah

--- datadiri/biodatasekolah.js
+++ datadiri/biodatasekolah.js
@@ -30,6 +30,102 @@ function getCookieData(name) {
     return null;
 }
 
+// Untuk POST prodi & fakultas
+// Membuat fungsi untuk mengirimkan data pilih prodi ke API
+function SubmitBiodataOrtu() {
+  const nisn = getValue('nisn');
+  const asal_jurusan = getValue('selectjurusan');
+  const asal_sekolah = "SMA";
+  const alamat = getValue('alamat');
+  const provinsi_sekolah = getValue('provinsi-biodata');
+  const kota_sekolah = getValue('kota-biodata');
+  const kode_pos_sekolah = getValue('kodepos');
+  const jenis_sekolah = getValue('selectjenis');
+  const akreditasi_sekolah = getValue('akred');
+  const tahun_lulus = getValue('tahun');
+
+  const studentData = {
+      "nisn": nisn,
+      "asal_jurusan": asal_jurusan,
+      "asal_sekolah": asal_sekolah,
+      "alamat_sekolah": alamat,
+      "provinsi_sekolah": provinsi_sekolah,
+      "kota_sekolah": kota_sekolah,
+      "kode_pos_sekolah": kode_pos_sekolah,
+      "jenis_sekolah": jenis_sekolah,
+      "jurusan": "jurusan",
+      "akreditasi_sekolah": akreditasi_sekolah,
+      "tahun_lulus": parseInt(tahun_lulus),
+      "guru_bk": "Nama BK",
+      "hp_guru_bk": "0981381283122"
+  };
+  
+  // Now 'studentData' is a constant containing the given JSON object
+  console.log(studentData);
+  
+
+  fetch(UrlPostDataSekolah, {
+      method : "POST",
+      headers : header,
+      body : JSON.stringify(studentData)
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          Swal.fire({
+              icon : 'success',
+              title : 'Sukses!',
+              text : 'Program studi berhasil disubmit.',
+              showConfirmButton : false,
+              timer : 1500
+          }).then(() => {
+              window.location.href = 'persyaratan.html';
+          });
+      } else {
+          Swal.fire({
+              icon : 'error',
+              title : 'Oops...',
+              text : 'Jalur pendaftaran gagal disubmit.'
+          })
+      }
+  })
+  .catch(error => {
+      console.error("Error saat melakukan POST Data : ", error);
+  });
+}
+
+// Event listener untuk tombol "Submit"
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', () => {
+  // const provinsi = getValue('selectprovince');
+  // const religion = getValue('selectreligion');
+  // const kotakab = getValue('selectkotakab');
+
+  // if (!provinsi || !religion || !kotakab ) {
+  //     Swal.fire({
+  //         icon: 'warning',
+  //         title: 'Oops...',
+  //         text: 'Semua field harus diisi!',
+  //     });
+  //     return;
+  // }
+  // Add additional validation if needed
+  Swal.fire({
+      title: 'Submit Jalur Pendaftaran?',
+      text: 'Apakah anda yakin ingin submit jalur pendaftaran?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+      cancelButtonText: 'No',
+  }).then((result) => {
+      if (result.isConfirmed) {
+          SubmitBiodataOrtu();
+      }
+  });
+});
+
 // Get Pekerjaan Jenis Sekolah
 function fecthDataJenisSekolah() {
   get(UrlGetJenisSekolah, populateDropdownJenisSekolah);
@@ -399,98 +495,3 @@ inputKota.addEventListener("input", async () => {
 //   }
 // });
 
-// Untuk POST prodi & fakultas
-// Membuat fungsi untuk mengirimkan data pilih prodi ke API
-function SubmitBiodataOrtu() {
-    const nisn = getValue('nisn');
-    const asal_jurusan = getValue('selectjurusan');
-    const asal_sekolah = "SMA";
-    const alamat = getValue('alamat');
-    const provinsi_sekolah = getValue('provinsi-biodata');
-    const kota_sekolah = getValue('kota-biodata');
-    const kode_pos_sekolah = getValue('kodepos');
-    const jenis_sekolah = getValue('selectjenis');
-    const akreditasi_sekolah = getValue('akred');
-    const tahun_lulus = getValue('tahun');
-
-    const studentData = {
-        "nisn": nisn,
-        "asal_jurusan": asal_jurusan,
-        "asal_sekolah": asal_sekolah,
-        "alamat_sekolah": alamat,
-        "provinsi_sekolah": provinsi_sekolah,
-        "kota_sekolah": kota_sekolah,
-        "kode_pos_sekolah": kode_pos_sekolah,
-        "jenis_sekolah": jenis_sekolah,
-        "jurusan": "jurusan",
-        "akreditasi_sekolah": akreditasi_sekolah,
-        "tahun_lulus": parseInt(tahun_lulus),
-        "guru_bk": "Nama BK",
-        "hp_guru_bk": "0981381283122"
-    };
-    
-    // Now 'studentData' is a constant containing the given JSON object
-    console.log(studentData);
-    
-
-    fetch(UrlPostDataSekolah, {
-        method : "POST",
-        headers : header,
-        body : JSON.stringify(studentData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            Swal.fire({
-                icon : 'success',
-                title : 'Sukses!',
-                text : 'Program studi berhasil disubmit.',
-                showConfirmButton : false,
-                timer : 1500
-            }).then(() => {
-                window.location.href = 'persyaratan.html';
-            });
-        } else {
-            Swal.fire({
-                icon : 'error',
-                title : 'Oops...',
-                text : 'Jalur pendaftaran gagal disubmit.'
-            })
-        }
-    })
-    .catch(error => {
-        console.error("Error saat melakukan POST Data : ", error);
-    });
-}
-
-// Event listener untuk tombol "Submit"
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', () => {
-    // const provinsi = getValue('selectprovince');
-    // const religion = getValue('selectreligion');
-    // const kotakab = getValue('selectkotakab');
-
-    // if (!provinsi || !religion || !kotakab ) {
-    //     Swal.fire({
-    //         icon: 'warning',
-    //         title: 'Oops...',
-    //         text: 'Semua field harus diisi!',
-    //     });
-    //     return;
-    // }
-    // Add additional validation if needed
-    Swal.fire({
-        title: 'Submit Jalur Pendaftaran?',
-        text: 'Apakah anda yakin ingin submit jalur pendaftaran?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            SubmitBiodataOrtu();
-        }
-    });
-});
