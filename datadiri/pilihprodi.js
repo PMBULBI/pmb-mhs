32bc24e4b02889634dbcaf07c1be2bcb1d9c6a22
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-07 15:17:33 +0700

    pindah function pilih prodi js

--- datadiri/pilihprodi.js
+++ datadiri/pilihprodi.js
@@ -8,20 +8,6 @@ var header = new Headers();
 header.append("login", token);
 header.append("Content-Type", "application/json");
 
-let dataJalur;
-const setValue = ( res ) =>{
-    if (res.data == null){
-        return;
-    };
-    dataJalur = res;
-    return
-} 
-function fetchDataBiodataJalur(){
-    getWithHeader(UrlBiodataJalur, TokenHeader, getCookie(CookieName), setValue);
-}
-
-fetchDataBiodataJalur();
-
 // Untuk POST prodi & fakultas
 // Membuat fungsi untuk mengirimkan data pilih prodi ke API
 function submitPilihProdi() {
@@ -95,6 +81,20 @@ submitButton.addEventListener('click', () => {
         }
     });
 })
+
+// Untuk Get Data Biodata Jalur
+let dataJalur;
+const setValue = ( res ) =>{
+    if (res.data == null){
+        return;
+    };
+    dataJalur = res;
+    return
+} 
+function fetchDataBiodataJalur(){
+    getWithHeader(UrlBiodataJalur, TokenHeader, getCookie(CookieName), setValue);
+}
+fetchDataBiodataJalur();
 
 // Get Program Studi 1
 // Membuat fungsi untuk fetch data prodi
