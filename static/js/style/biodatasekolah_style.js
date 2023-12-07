ac8898f584f8c2247def3be7bc261a2032f94511
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-07 15:36:25 +0700

    style for kode pos in biodata sekolah

--- static/js/style/biodatasekolah_style.js
+++ static/js/style/biodatasekolah_style.js
@@ -11,3 +11,17 @@ nisnInput.addEventListener("input", function() {
     nisnInput.value = sanitizedInput;
   }
 });
+
+// Inputkan untuk Kode Pos
+const kodePosInput = document.getElementById("kodepos");
+// Menambahkan event listener untuk memvalidasi input
+kodePosInput.addEventListener("input", function() {
+  // Menghapus karakter selain angka dari input
+  const sanitizedInput = kodePosInput.value.replace(/[^0-9]/g, '');
+  // Membatasi panjang input menjadi maksimal 5 angka
+  if (sanitizedInput.length > 5) {
+    kodePosInput.value = sanitizedInput.slice(0, 5);
+  } else {
+    kodePosInput.value = sanitizedInput;
+  }
+});
