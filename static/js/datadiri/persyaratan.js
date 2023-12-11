9ecf5d63b182abc4cdb5c39ff35de6b22311123a
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-11 14:29:56 +0700

    coba uploud file persyaratan

--- static/js/datadiri/persyaratan.js
+++ static/js/datadiri/persyaratan.js
@@ -3,6 +3,27 @@ import { getValue, setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/elem
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
 import { token } from "../controller/cookies.js";
+import {setInner, show,hide,getValue,getFileSize} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
+import { postFile} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
+
+window.uploadFile = uploadFile;
+
+function uploadFile() {
+    if (!getValue("imageInput")) {
+        alert('Please select an image file');
+        return;
+    }
+    hide("inputfile");
+    let besar=getFileSize("imageInput");
+    setInner("isi",besar);
+    postFileWithHeader(UrlFilePost, "login", token,"file_input","file",renderToHtml);
+}
+
+function renderToHtml(result){
+    console.log(result);
+    // setInner("isi",result.content);
+    // show("inputfile");
+}
 
 // Get Data Cookies
 // Get Untuk Data di Navbar dan Form
@@ -13,46 +34,46 @@ function renderDataPendaftar(result){
   }
 }
 
-// Event listener for the "Submit" button
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', () => {
-    const form = document.getElementById("form");
-    const inputFile = document.getElementById("file_input");
+// // Event listener for the "Submit" button
+// const submitButton = document.getElementById('submitButton');
+// submitButton.addEventListener('click', () => {
+//     const form = document.getElementById("form");
+//     const inputFile = document.getElementById("file_input");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+//     const handleSubmit = async (event) => {
+//         event.preventDefault();
 
-        const formData = new FormData();
+//         const formData = new FormData();
 
-        for (const file of inputFile.files) {
-            formData.append("file", file);
-        }
+//         for (const file of inputFile.files) {
+//             formData.append("file", file);
+//         }
 
-        try {
-            const response = await fetch(UrlFilePost, {
-                method: "POST",
-                headers: {
-                    'login': token,
-                },
-                body: formData,
-            })
+//         try {
+//             const response = await fetch(UrlFilePost, {
+//                 method: "POST",
+//                 headers: {
+//                     'login': token,
+//                 },
+//                 body: formData,
+//             })
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
+//             if (!response.ok) {
+//                 throw new Error(`HTTP error! Status: ${response.status}`);
+//             }
 
-            const data = await response.json();
-            if (!data.success){
-                throw new Error(`HTTP error! Status: ${data.status}`);
-            }
+//             const data = await response.json();
+//             if (!data.success){
+//                 throw new Error(`HTTP error! Status: ${data.status}`);
+//             }
 
-            console.log("Response data:", data);
-            window.location.replace("https://pmb.ulbi.ac.id/pmb-mhs/VAPage.html");
-        } catch (error) {
-            console.error("Error saat melakukan POST Data:", error);
-        }
-    };
+//             console.log("Response data:", data);
+//             window.location.replace("https://pmb.ulbi.ac.id/pmb-mhs/VAPage.html");
+//         } catch (error) {
+//             console.error("Error saat melakukan POST Data:", error);
+//         }
+//     };
 
-    // Event listener for form submission
-    //form.addEventListener("submit", handleSubmit);
-});
+//     // Event listener for form submission
+//     //form.addEventListener("submit", handleSubmit);
+// });
