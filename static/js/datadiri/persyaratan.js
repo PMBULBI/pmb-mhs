50291d8d794596034897f14df5342621cbfadf6b
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-11 15:15:26 +0700

    clear code ga penting di persyaratan

--- static/js/datadiri/persyaratan.js
+++ static/js/datadiri/persyaratan.js
@@ -1,19 +1,11 @@
 import { UrlFilePost, UrlGetDataPendaftar } from "../controller/template.js";
-import { getValue, setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.2/croot.js";
+import { setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.2/croot.js";
 import { token } from "../controller/cookies.js";
-// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
 import { postFileWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
 
 // Get Data Cookies
 // Get Untuk Data di Navbar dan Form
-// document.addEventListener("DOMContentLoaded", function() {
-//     var namaMhs = getCookie('namaMhs');
-  
-//     if (namaMhs) {
-//         setInnerText('nama_mhs_span', namaMhs);
-//     }
-// });
 getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
 function renderDataPendaftar(result){
   if (result.success){
@@ -30,46 +22,3 @@ submitButton.addEventListener('click', () => {
 function renderToHtml(result) {
     console.log(result);
 };
-
-// const submitButton = document.getElementById('submitButton');
-// submitButton.addEventListener('click', () => {
-//     const form = document.getElementById("form");
-//     const inputFile = document.getElementById("file_input");
-
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-
-//         const formData = new FormData();
-
-//         for (const file of inputFile.files) {
-//             formData.append("file", file);
-//         }
-
-//         try {
-//             const response = await fetch(UrlFilePost, {
-//                 method: "POST",
-//                 headers: {
-//                     'login': token,
-//                 },
-//                 body: formData,
-//             })
-
-//             if (!response.ok) {
-//                 throw new Error(`HTTP error! Status: ${response.status}`);
-//             }
-
-//             const data = await response.json();
-//             if (!data.success){
-//                 throw new Error(`HTTP error! Status: ${data.status}`);
-//             }
-
-//             console.log("Response data:", data);
-//             window.location.replace("https://pmb.ulbi.ac.id/pmb-mhs/VAPage.html");
-//         } catch (error) {
-//             console.error("Error saat melakukan POST Data:", error);
-//         }
-//     };
-
-//     // Event listener for form submission
-//     form.addEventListener("submit", handleSubmit);
-// });
