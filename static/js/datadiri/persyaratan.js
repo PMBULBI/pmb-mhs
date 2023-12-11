79bfbacb26e4cd476190da66ed0a07bbe3afef6c
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-11 14:59:01 +0700

    ganti code untuk navbar

--- static/js/datadiri/persyaratan.js
+++ static/js/datadiri/persyaratan.js
@@ -1,17 +1,24 @@
-import { UrlFilePost } from "../controller/template.js";
+import { UrlFilePost, UrlGetDataPendaftar } from "../controller/template.js";
 import { getValue, setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.2/croot.js";
 import { token } from "../controller/cookies.js";
-import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+// import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
 
 // Get Data Cookies
 // Get Untuk Data di Navbar dan Form
-document.addEventListener("DOMContentLoaded", function() {
-    var namaMhs = getCookie('namaMhs');
+// document.addEventListener("DOMContentLoaded", function() {
+//     var namaMhs = getCookie('namaMhs');
   
-    if (namaMhs) {
-        setInnerText('nama_mhs_span', namaMhs);
-    }
-});
+//     if (namaMhs) {
+//         setInnerText('nama_mhs_span', namaMhs);
+//     }
+// });
+getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
+function renderDataPendaftar(result){
+  if (result.success){
+    setInnerText('nama_mhs_span', result.data.nama_mhs);
+  }
+}
 
 // Event listener for the "Submit" button
 const submitButton = document.getElementById('submitButton');
