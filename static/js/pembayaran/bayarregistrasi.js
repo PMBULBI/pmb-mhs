cd0565c3e4dbbdf130e4806f2b087d8261059bae
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-11 17:08:37 +0700

    add library get with header

--- static/js/pembayaran/bayarregistrasi.js
+++ static/js/pembayaran/bayarregistrasi.js
@@ -1,7 +1,7 @@
 import { UrlGetDataPendaftar, UrlPostVA } from "../controller/template.js";
 import { postWithToken}  from "https://jscroot.github.io/api/croot.js";
-// import { getValue, addChild } from "https://jscroot.github.io/element/croot.js";
 import { token } from "../controller/cookies.js";
+import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
 import { setValue, setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
 
 // Get Untuk Data di Navbar dan Form
