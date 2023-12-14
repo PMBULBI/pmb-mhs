cc330f7abb4a67d9ed4678e7e4cbd7c4eaecb347
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-14 18:36:31 +0700

    add library post with token

--- static/js/pembayaran/lunasregistrasi.js
+++ static/js/pembayaran/lunasregistrasi.js
@@ -1,6 +1,7 @@
 import { UrlGetDataPendaftar, UrlCekPembayaranVAReg } from "../controller/template.js";
 import { token } from "../controller/cookies.js";
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+import { postWithToken}  from "https://jscroot.github.io/api/croot.js";
 import { setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
 
 // Cek Pembayaran VA Registrasi
