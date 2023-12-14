31995699ec6b20b2b4f9a8b5f10b4b53ed69fdc8
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-14 18:30:10 +0700

    feat(lunas) : lunas register

--- static/js/pembayaran/lunasregistrasi.js
+++ static/js/pembayaran/lunasregistrasi.js
@@ -1,12 +1,20 @@
-// Untuk Cetak Invoice Registrasi
-// function printInvoice() {
-//     // Open a new window or create an iframe
-//     var printWindow = window.open('', '_blank');
-    
-//     // Copy the HTML content of the targeted card to the new window or iframe
-//     var invoiceContent = document.querySelector('.card').outerHTML;
-//     printWindow.document.write('<html><head><title>Invoice</title></head><body>' + invoiceContent + '</body></html>');
-    
-//     // Call the print function in the new window or iframe
-//     printWindow.print();
-//   }
+import { UrlGetDataPendaftar, UrlCekPembayaranVAReg } from "../controller/template.js";
+import { token } from "../controller/cookies.js";
+import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+import { setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
+
+// Cek Pembayaran VA Registrasi
+postWithToken(UrlCekPembayaranVAReg, "LOGIN", token, "test", ResponseVAReg)
+function ResponseVAReg(value) {
+  if (!value.success) {
+    window.location.replace("bayarregistrasi.html");
+  }
+}
+
+// Get Untuk Data
+getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
+function renderDataPendaftar(result){
+  if (result.success){
+    setInnerText('nama_mhs_span', result.data.nama_mhs);
+  }
+}
