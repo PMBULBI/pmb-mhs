f89760ddc98230db31d6689f03103e499d23fe24
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-14 15:49:49 +0700

    create function cek pembayaran va reg

--- static/js/pembayaran/bayarregistrasi.js
+++ static/js/pembayaran/bayarregistrasi.js
@@ -1,8 +1,21 @@
-import { UrlGetDataPendaftar, UrlPostVA } from "../controller/template.js";
+import { UrlGetDataPendaftar, UrlPostVA, UrlCekPembayaranVAReg } from "../controller/template.js";
 import { postWithToken}  from "https://jscroot.github.io/api/croot.js";
 import { token } from "../controller/cookies.js";
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
-import { setValue, setInnerText } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
+import { setValue, setInnerText, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.5/croot.js";
+
+// Cek Pembayaran VA Registrasi
+postWithToken(UrlCekPembayaranVAReg, "LOGIN", token, "test", ResponseVAReg)
+
+function ResponseVAReg(value) {
+  if (value.success) {
+    setInner('alertPembayaran', 'Pembayaran Registrasi Anda Telah Lunas');
+    // Show the hidden button
+    document.getElementById('buttonCetakInvoice').classList.remove('hidden');
+  } else {
+    setInner('alertPembayaran', 'Silahkan Selesaikan Pembayaran Registrasi Anda');
+  }
+}
 
 // Get Untuk Data di Navbar dan Form
 getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
@@ -12,9 +25,9 @@ function renderDataPendaftar(result){
   }
 }
 
-function fetchVA() {
-    postWithToken(UrlPostVA, "LOGIN", token,  "test" ,Response)
-}
+
+postWithToken(UrlPostVA, "LOGIN", token,  "test" ,Response)
+
 
 function Response(value){
   if (value.success){
@@ -24,9 +37,7 @@ function Response(value){
     setValue('nohpInput', value.data.phone)
     setValue('vaInput', value.data.virtual_account);
     setInnerText('nominalRegis1', value.data.amount);
-    setInnerText('nominalRegis2', value.data.amount);
     setInnerText('expiredDateInput', value.data.datetime_expired);
-    setInnerText('nominalAlert', value.data.amount)
   } else {
     Swal.fire({ 
       icon : 'error',
@@ -36,5 +47,3 @@ function Response(value){
     console.log(value);
   }
 }
-
-fetchVA();
