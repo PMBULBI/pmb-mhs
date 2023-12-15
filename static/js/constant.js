385d82508be850c6a6cb29213316642e31d62ad7
JPratama7 <josepratama080@gmail.com>
2023-12-15 10:28:19 +0700

    fix: move baseurl ke variable

---
+++ static/js/constant.js
@@ -0,0 +1,1 @@
+export const BaseUrl = "https://komarbe.ulbi.ac.id";
--- static/js/controller/kuesioner.js
+++ static/js/controller/kuesioner.js
@@ -1,11 +1,12 @@
 import { postWithToken} from "https://jscroot.github.io/api/croot.js";
 import {getValue, addChild} from "https://jscroot.github.io/element/croot.js";
+import { BaseUrl } from "../constant.js";
 import { token } from "./cookies.js";
 
 
 
 const main = async () =>{
-    postWithToken("https://komarbe.ulbi.ac.id/kuesioner", "LOGIN", token, "test", responseKuesioner)
+    postWithToken(`${BaseUrl}/kuesioner`, "LOGIN", token, "test", responseKuesioner)
 }
 
 
