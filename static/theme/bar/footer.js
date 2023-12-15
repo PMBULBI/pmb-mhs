118d60504aa6789449e19062e0a87d94e622b36c
Rolly Maulana  Awangga <rolly@awang.ga>
2023-12-15 10:15:34 +0700

    add(js):static theme js pindah ke lokal

---
+++ static/theme/bar/footer.js
@@ -0,0 +1,5 @@
+$(function () {
+    // Load navbar.html on page load
+    $("#footer-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.1.7/static/footer.html");
+  
+  });
---
+++ static/theme/bar/sidebar.js
@@ -0,0 +1,4 @@
+$(function () {
+    // Load navbar.html on page load
+    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.3.6/static/sidebar.html");
+  });
