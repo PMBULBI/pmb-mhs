d1088a5e9cef94d915d2bc7f6953541154d56542
Rolly Maulana  Awangga <rolly@awang.ga>
2023-12-15 10:34:21 +0700

    upgrade(footer): pakai ke versi naik ke 3.7

--- static/theme/bar/footer.js
+++ static/theme/bar/footer.js
@@ -1,5 +1,5 @@
 $(function () {
     // Load navbar.html on page load
-    $("#footer-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.1.7/static/footer.html");
+    $("#footer-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.3.7/static/footer.html");
   
   });
--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,4 +1,4 @@
 $(function () {
     // Load navbar.html on page load
-    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.3.6/static/sidebar.html");
+    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.3.7/static/sidebar.html");
   });
