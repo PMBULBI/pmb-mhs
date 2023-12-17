3493917be2351cb8a5b82590339fb7a9c92ab14b
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-18 06:26:46 +0700

    upgrade:4..0 ke 4.1 js footer dan sidebar

--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,4 +1,4 @@
 $(function () {
     // Load navbar.html on page load
-    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.4.0/static/sidebar.html");
+    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.4.1/static/sidebar.html");
   });
