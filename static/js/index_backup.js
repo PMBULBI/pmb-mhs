69e12142468ac4abd275475af8d15c2eb48cbd58
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-18 18:50:52 +0700

    add token in index_backup

--- static/js/index_backup.js
+++ static/js/index_backup.js
@@ -1,6 +1,10 @@
 import { UrlGetBiodataJalurWithToken } from "./controller/template.js";
 import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+import { token } from "../controller/cookies.js";
 
+var header = new Headers();
+header.append("login", token);
+header.append("Content-Type", "application/json");
 // Get Untuk Data di Navbar dan Form
 getWithHeader(UrlGetBiodataJalurWithToken, "login", token, responseData);
 function responseData(result){
