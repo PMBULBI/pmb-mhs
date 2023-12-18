9fd71740d9fd26106649f51f5ac0e4f729586f10
Rolly Maulana  Awangga <rolly@awang.ga>
2023-12-18 16:32:08 +0700

    sidebar.html

--- static/theme/bar/sidebar.js
+++ static/theme/bar/sidebar.js
@@ -1,4 +1,4 @@
 $(function () {
     // Load navbar.html on page load
-    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.4.4/static/sidebar.html");
+    $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.4.5/static/sidebar.html");
   });
