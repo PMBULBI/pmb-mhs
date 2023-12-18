db5e2e8623e3ee9074e5febf9e6bb5e6969865f6
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-18 08:56:34 +0700

    add(header):name replace

--- static/js/controller/kuesioner.js
+++ static/js/controller/kuesioner.js
@@ -1,19 +1,19 @@
-import { postWithToken, getWithHeader } from "https://jscroot.github.io/api/croot.js";
-import { getValue, getValueRadio, onClick } from "https://jscroot.github.io/element/croot.js";
+import { postWithToken, getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
+import { getValue, getValueRadio, onClick } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.0/croot.js";
 import { UrlGetDataPendaftar } from "./template.js";
 import { BaseUrl } from "../constant.js";
 import { token } from "./cookies.js";
 
-// Get Untuk Data di Navbar dan Form
-// getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
-// function renderDataPendaftar(result){
-//   if (result.success){
-//     setInnerText('nama_mhs_span', result.data.nama_mhs);
+//Get Untuk Data di Navbar dan Form
+getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
+function renderDataPendaftar(result){
+  if (result.success){
+    setInnerText('nama_mhs_span', result.data.nama_mhs);
+  }
+//   else{
+//     window.location.replace("https://pmb.ulbi.ac.id/");
 //   }
-// //   else{
-// //     window.location.replace("https://pmb.ulbi.ac.id/");
-// //   }
-// }
+}
 
 const main = () => {
     onClick("buttonSave", runAll);
