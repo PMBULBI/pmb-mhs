ab02a6830a7e35335c16f5bf5ac672c836f68aec
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-18 17:11:16 +0700

    feat : konfirmasi sudah isi kuesioner atau belum

--- static/js/controller/kuesioner.js
+++ static/js/controller/kuesioner.js
@@ -1,8 +1,23 @@
 import { postWithToken, getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
 import { setInnerText, getValueRadio, onClick } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.0/croot.js";
-import { UrlGetDataPendaftar } from "./template.js";
+import { UrlGetDataPendaftar, UrlGetKuesionerWithToken } from "./template.js";
 import { BaseUrl } from "../constant.js";
 import { token } from "./cookies.js";
+
+// Cek Pengisian Kuesioner
+postWithToken(UrlGetKuesionerWithToken, "LOGIN", token, "test", ResponseGetKuesioner)
+
+function ResponseGetKuesioner(value) {
+    const bgWarning = "py-[18px] px-6 font-normal font-Inter text-sm rounded-md bg-warning-500 text-white dark:bg-warning-500 dark:text-slate-300 mb-2";
+    const bgSuccess = "py-[18px] px-6 font-normal font-Inter text-sm rounded-md bg-success-500 text-white dark:bg-success-500 dark:text-slate-300 mb-2";
+    if (value.data.success) {
+        setInner('alertKuesioner', '<b>Sudah Lunas!</b> Terima kasih kamu telah melakukan <b>pembayaran registrasi</b>.');
+        document.getElementById("colorAlert").className = bgSuccess;
+    } else {
+        setInner('alertKuesioner', 'Silahkan lakukan <b>pembayaran registrasi</b> sesuai biaya yang tertera di bawah.');
+    document.getElementById("colorAlert").className = bgWarning;
+    }
+}
 
 //Get Untuk Data di Navbar dan Form
 getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
