07d02e2070dcf85c617e700dea96fbbef30b2c56
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-18 18:40:48 +0700

    coba ubah rute aplikasi

---
+++ static/js/index_backup.js
@@ -0,0 +1,12 @@
+import { UrlGetBiodataJalurWithToken, UrlGetBiodataJalurWithToken } from "./controller/template";
+import { getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.1/croot.js";
+
+// Get Untuk Data di Navbar dan Form
+getWithHeader(UrlGetBiodataJalurWithToken, "login", token, responseData);
+function responseData(result){
+  if (result.success){
+    window.location.replace("https://pmb.ulbi.ac.id/datadiri/jalurpendaftaran.html")
+  }else{
+    window.location.replace("https://pmb.ulbi.ac.id/index_backup.html");
+  }
+}
