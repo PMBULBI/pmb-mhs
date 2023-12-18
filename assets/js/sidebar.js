1744fb4d74b869f1573a040f86d9638f5642fca4
Rolly Maulana  Awangga <rolly@awang.ga>
2023-12-18 16:24:49 +0700

    fix: hide html sidebar

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,5 +1,5 @@
 $(function () {
   // Load navbar.html on page load
-  $("#sidebar-container").load("https://raw.githubusercontent.com/PMBULBI/pmb-mhs/main/static/sidebar.html");
+  $("#sidebar-container").load("https://cdn.jsdelivr.net/gh/PMBULBI/javascript-fe@0.4.4/static/sidebar.html");
 
 });
