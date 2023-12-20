38dd03e1fef1aa45c7a3692d06ef7b924264d7bc
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-21 00:41:22 +0700

    clear code kuesioner.js

--- static/js/kuesioner/kuesioner.js
+++ static/js/kuesioner/kuesioner.js
@@ -1,19 +1,8 @@
 import { postWithToken, getWithHeader } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
-import { setInnerText, getValueRadio, onClick, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.0/croot.js";
-import { UrlGetDataPendaftar, UrlGetKuesionerWithToken } from "../controller/template.js";
+import { setInnerText, getValueRadio, onClick } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.0/croot.js";
+import { UrlGetDataPendaftar } from "../controller/template.js";
 import { BaseUrl } from "../constant.js";
 import { token } from "../controller/cookies.js";
-
-// Cek Pengisian Kuesioner
-getWithHeader(UrlGetKuesionerWithToken, "login", token, ResponseGetKuesioner)
-
-function ResponseGetKuesioner(value) {
-    if (value.success) {
-        window.location.replace('https://pmb.ulbi.ac.id/pmb-mhs/kuesioner/konfirmasikuesioner.html');
-    } else {
-        console.log(value);
-    }
-}
 
 //Get Untuk Data di Navbar dan Form
 getWithHeader(UrlGetDataPendaftar,"login",token,renderDataPendaftar);
