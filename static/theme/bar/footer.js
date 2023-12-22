11eb2ddf073570707d2713efc1c7486e61c2b519
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-12-22 14:19:20 +0700

    clear code

--- static/theme/bar/footer.js
+++ static/theme/bar/footer.js
@@ -1,9 +1,5 @@
+// Untuk Render Footer
 document.addEventListener('DOMContentLoaded', function() {
   $("#footer-container").load("https://pmb.ulbi.ac.id/pmb-mhs/static/footer.html");
   console.log('DOM fully loaded and parsed');
 });
-
-// $(function () {
-//     // Load navbar.html on page load
-//     $("#footer-container").load("https://pmb.ulbi.ac.id/pmb-mhs/static/footer.html");
-//   });
